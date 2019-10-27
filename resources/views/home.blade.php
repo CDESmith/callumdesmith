51fb7a2916a1b9382f976e89f2a42768eab42d88
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2019-10-27 19:36:13 +0000

    Populated head with style and script links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+    <a class="menu-toggle rounded" href="#">
+        <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <a class="js-scroll-trigger" href="{{url('/')}}">{{config('app.name')}}</a>
+            </li>
+            <li class="sidebar-nav-item">
+                <a class="js-scroll-trigger" href="#page-top">Home</a>
+            </li>
+        </ul>
+    </nav>
+    <header class="masthead d-flex">
+        <div class="container text-center my-auto">
+            <h1 class="mb-1">{{config('app.name')}}</h1>
+            <h3 class="mb-5"><em>{{config('app.subtitle')}}</em></h3>
+        </div>
+        <div class="overlay"></div>
+    </header>
+    <footer class="footer text-center">
+        <div class="container">
+            <ul class="list-inline mb-5">
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white" href="#">
+                        <i class="icon-social-github"></i>
+                    </a>
+                </li>
+            </ul>
+            <p class="text-muted small mb-0">Copyright &copy; <a href="{{config('app.url')}}">{{config('app.name')}}</a> {{now()->year}}.</p>
+        </div>
+    </footer>
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,25 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{config('app.url')}}">
     <meta property="og:image" content=""> <!-- 200x200px - 1200x1200px -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="{{asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151008366-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-151008366-1');
+    </script>
 </head>
-<body>
+<body id="page-top">
     @yield('content')
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('js/app.min.js')}}"></script>
 </body>
 </html>
