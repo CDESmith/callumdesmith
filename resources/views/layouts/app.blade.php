8e4d1004ab719f200b8af0e8ea27477dd7611fe9
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2019-10-27 19:22:09 +0000

    Populated head with config variables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" prefix="og: http://ogp.me/ns#">
 <head>
     <meta charset="utf-8">
-    <meta name="author" content="">
-    <meta name="description" content="">
+    <meta name="author" content="{{config('app.name')}}">
+    <meta name="description" content="{{config('app.subtitle')}}">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
-    <meta property="og:site_name" content="">
+    <title>{{config('app.name')}} | {{config('app.subtitle')}}</title>
+    <meta property="og:title" content="{{config('app.name')}} | {{config('app.subtitle')}}">
+    <meta property="og:description" content="{{config('app.subtitle')}}">
+    <meta property="og:site_name" content="{{config('app.name')}}">
     <meta property="og:locale" content="en_gb">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="">
+    <meta property="og:url" content="{{config('app.url')}}">
     <meta property="og:image" content=""> <!-- 200x200px - 1200x1200px -->
 </head>
 <body>
