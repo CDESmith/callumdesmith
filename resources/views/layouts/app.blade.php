2481dfc1b6441ac13c27f62322a66d61d7d9f03c
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2019-10-27 18:00:00 +0000

    Template files setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" prefix="og: http://ogp.me/ns#">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:locale" content="en_gb">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content=""> <!-- 200x200px - 1200x1200px -->
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
